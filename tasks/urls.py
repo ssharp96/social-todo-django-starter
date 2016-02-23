6d9fdf79c9c8ffcafcab6f076945fbf410b0c96c
ssharp96 <simonsharp96@gmail.com>
2016-02-23 03:29:56 +0000

    login now properly takes user to /tasks page... success!

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.tasks, name='tasks'),
+    # url(r'^create', views.create, name='create')
 ]
 
--- tasks/views.py
+++ tasks/views.py
@@ -6,7 +6,7 @@ from tasks.models import *
 
 # Create your views here.
 
-def index(request):
+def tasks(request):
     tasks = Task.objects.all()
     current_user = request.user
     # tasks[] = 
@@ -15,10 +15,11 @@ def index(request):
             
         
     if current_user.is_authenticated():
-        template = loader.get_template('tasks/index.html')
+        template = loader.get_template('tasks/tasks.html')
         context = {'tasks': tasks}
-        return render(request, "tasks/index.html", {'context': context})
+        return render(request, "tasks/tasks.html", {'context': context})
     else:
+        print("what the fuck am i doing here?")
         return HttpResponseRedirect('/')
     
 # def create(request):
