20739f564c240a4786dedb5f6605c7fbe84f66bd
ssharp96 <simonsharp96@gmail.com>
2016-02-23 16:33:58 +0000

    commit w David

--- tasks/views.py
+++ tasks/views.py
@@ -11,6 +11,11 @@ from django.db import models
 # Create your views here.
 
 def tasks(request):
+    
+    # Instead of doing a query for all tasks here, what you want to do
+    # is a search for all tasks where either a) the task is owned by the
+    # current user or b) the current user is a collaborator on the task.
+    # You might have to looking into Django's "Q" object for this query
     all_tasks = Task.objects.all()
     current_user = request.user
     tasks = []
@@ -26,18 +31,31 @@ def tasks(request):
     
 def create(request):
     if request.method == 'POST':
-        print("gets here 1")
         user = request.user
         owner = user
         title = request.POST['title']
         description = request.POST['description']
-        # collaborators = request.POST['confirm_password']
-        is_complete = False
+        collab_emails = []
+        print("gets here 1")
+        collab_emails.append(request.POST['collaborators1'].lower())
+        collab_emails.append(request.POST['collaborators2'].lower())
+        collab_emails.append(request.POST['collaborators3'].lower())
         print("gets here 2")
-        task = Task(owner=owner, title=title, description=description, is_complete=False)
+        collaborators = []
         print("gets here 3")
+        # 1. Grab the collaborator emails addresses from the submitted form
+        # 2. turn them into lowercase
+        # 3. Look up all Users that have those email addresses
+        # 4. For those that are found, make them collaborators on this new task
+        # 5. Save the task
+        
+        is_complete = False
+        task = Task(owner=owner, title=title, description=description, is_complete=False)
         if task is not None:
             task.save()
+            for email in collab_emails:
+                collab = User.objects.get(email=email)
+                task.collaborators.add(collab)
             return HttpResponseRedirect('/tasks/')
         else:
             return render(request, 'tasks/tasks.html', {'errors':"Make sure you fill in all forms!"})
