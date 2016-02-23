d74031307d8bea69e160fe8614f7197c8323cb9e
ssharp96 <simonsharp96@gmail.com>
2016-02-23 02:38:58 +0000

    login works, need to fix URLs to display tasks when user logs in

--- tasks/views.py
+++ tasks/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.template.loader import get_template
+from tasks.models import *
 
 # Create your views here.
 
 def index(request):
     tasks = Task.objects.all()
-    template = loader.get_template('tasks/index.html')
-    context = {'tasks': tasks,}
-    return HttpResponse(template.render(context, request))
+    current_user = request.user
+    # tasks[] = 
+    # for task in all_tasks:
+    #     if task.owner == current_user:
+            
+        
+    if current_user.is_authenticated():
+        template = loader.get_template('tasks/index.html')
+        context = {'tasks': tasks}
+        return render(request, "tasks/index.html", {'context': context})
+    else:
+        return HttpResponseRedirect('/')
     
-# def create(request, request.session['username']):
-#     var current_user = request.session['username]
+# def create(request):
+    
     
